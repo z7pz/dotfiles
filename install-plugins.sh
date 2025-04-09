5d189672f37aab864b31c9ac67d00fc69e650cb2
z7pz <sdfygamer@gmail.com>
2025-04-09 22:43:01 +0300

    feat: add install plugins command

---
+++ install-plugins.sh
@@ -0,0 +1,5 @@
+# Clone all plugins under ~/.zsh/plugins/
+mkdir -p ~/.zsh/plugins
+git clone https://github.com/mafredri/zsh-async ~/.zsh/plugins/zsh-async
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/plugins/zsh-history-substring-search
